27e564634a9bc40ed1bd35994509e729e5aaa1fa
Siegfried Margin <siegfried.margin@gmail.com>
2024-05-17 16:17:27 +0200

    Added: add first eventEmitter

--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-game-control',
   templateUrl: './game-control.component.html',
-  styleUrl: './game-control.component.css'
+  styleUrl: './game-control.component.css',
 })
-export class GameControlComponent {
+export class GameControlComponent implements OnInit {
+  @Output() eventStart = new EventEmitter();
+  constructor() {}
 
+  ngOnInit() {}
+  onStart() {
+    /*   this.emit((ref = setInterval())); */
+    /* ; */
+  }
+  onStop() {}
 }
